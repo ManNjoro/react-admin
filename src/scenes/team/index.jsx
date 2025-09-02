eab4cd85bed1a806d8aa4050aeba2e6192aadff6
ManNjoro <elijohnmwoho@gmail.com>
2025-09-02 12:25:33 +0300

    Added import statements and initial Team component in team scene

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined'
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined'
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined'
+
+const Team = () => {
+  return (
+    <div>Team</div>
+  )
+}
+
+export default Team
